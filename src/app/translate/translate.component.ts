f4c3d1503364e2889529e4a369024f6e7802c86c
rivka <rivkaaveta040997@gmail.com>
2024-02-28 14:17:28 +0200

    asf

--- src/app/translate/translate.component.ts
+++ src/app/translate/translate.component.ts
@@ -1,8 +1,10 @@
-import { Component,OnInit } from '@angular/core';
+import { Component,Input,OnInit } from '@angular/core';
 import {MatInputModule} from '@angular/material/input';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {FormsModule} from '@angular/forms';
 import { RouterLink } from '@angular/router';
+import { Category } from '../shared/model/category';
+import { CategoryService } from '../services/category.service';
 @Component({
   selector: 'app-translate',
   standalone: true,
@@ -11,8 +13,14 @@ import { RouterLink } from '@angular/router';
   styleUrl: './translate.component.css'
 })
 export class TranslateComponent implements OnInit {
+  @Input()identifier?: number;
+  categories: Category[] = [];
+  constructor(private categoryService: CategoryService) {
+    
+  }
   ngOnInit(): void {
     throw new Error('Method not implemented.');
   }
 
 }
+
